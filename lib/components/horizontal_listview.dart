41764c6d66f7581eda44e5a15979e7446486709a
Ezzin Houssam <ezzinhoussam@gmail.com>
2021-09-29 19:00:35 +0100

    app bar + home page

---
+++ lib/components/horizontal_listview.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class HorizontalList extends StatelessWidget {
+  const HorizontalList({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Category(
+              imageLocation: 'assets/images/cats/tshirt.png',
+              imageCaption: 'Shirt'),
+          Category(
+              imageLocation: 'assets/images/cats/accessories.png',
+              imageCaption: 'Accessories'),
+          Category(
+              imageLocation: 'assets/images/cats/dress.png',
+              imageCaption: 'Dress'),
+          Category(
+              imageLocation: 'assets/images/cats/formal.png',
+              imageCaption: 'Formal'),
+          Category(
+              imageLocation: 'assets/images/cats/jeans.png',
+              imageCaption: 'Jeans'),
+          Category(
+              imageLocation: 'assets/images/cats/shoe.png',
+              imageCaption: 'Shoe'),
+        ],
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  final String imageLocation;
+  final String imageCaption;
+
+  const Category(
+      {Key? key, required this.imageLocation, required this.imageCaption})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: InkWell(
+          onTap: () {
+            print(imageLocation);
+          },
+          child: Container(
+            width: 100.0,
+            child: ListTile(
+              title: Image.asset(
+                imageLocation,
+                width: 100,
+                height: 60,
+              ),
+              subtitle: Container(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  imageCaption,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:e_commerce_app/components/horizontal_listview.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,72 +12,106 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text('Shopo'),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                color: Colors.white,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.shopping_cart,
-                color: Colors.white,
-              ))
+    Widget imageCarousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('assets/images/m1.jpeg'),
+          AssetImage('assets/images/c1.jpg'),
+          AssetImage('assets/images/m2.jpg'),
+          AssetImage('assets/images/w1.jpeg'),
+          AssetImage('assets/images/w3.jpeg'),
+          AssetImage('assets/images/w4.jpeg'),
         ],
+        autoplay: false,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 8.0,
       ),
-      drawer: Drawer(
-          child: ListView(
-        children: [
-          //drawer header
-          UserAccountsDrawerHeader(
-            accountName: Text('Houssam Ezzin'),
-            accountEmail: Text('ezzinhoussam@gmail.com'),
-            currentAccountPicture: GestureDetector(
-                child: CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    child: Icon(Icons.person, color: Colors.white))),
-            decoration: BoxDecoration(color: Colors.redAccent),
-          ),
-          //drawer body
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('Home Page'), leading: Icon(Icons.home))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('My Account'), leading: Icon(Icons.person))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('My Orders'),
-                  leading: Icon(Icons.shopping_basket))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('Categories'), leading: Icon(Icons.dashboard))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('Favorites'), leading: Icon(Icons.favorite))),
-
-          Divider(),
-
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text('Settings'), leading: Icon(Icons.settings))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(title: Text('About'), leading: Icon(Icons.help))),
-        ],
-      )),
     );
+
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.redAccent,
+          title: Text('Shopo'),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        drawer: Drawer(
+            child: ListView(
+          children: [
+            //drawer header
+            UserAccountsDrawerHeader(
+              accountName: Text('Houssam Ezzin'),
+              accountEmail: Text('ezzinhoussam@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                  child: CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: Icon(Icons.person, color: Colors.white))),
+              decoration: BoxDecoration(color: Colors.redAccent),
+            ),
+            //drawer body
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('Home Page'), leading: Icon(Icons.home))),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('My Account'), leading: Icon(Icons.person))),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('My Orders'),
+                    leading: Icon(Icons.shopping_basket))),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('Categories'), leading: Icon(Icons.dashboard))),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('Favorites'), leading: Icon(Icons.favorite))),
+
+            Divider(),
+
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('Settings'),
+                    leading: Icon(Icons.settings, color: Colors.green))),
+            InkWell(
+                onTap: () {},
+                child: ListTile(
+                    title: Text('About'),
+                    leading: Icon(Icons.help, color: Colors.blueAccent))),
+          ],
+        )),
+        body: ListView(
+          children: [
+            imageCarousel,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Categories'),
+            ),
+            //Horizontal list view :
+            HorizontalList()
+          ],
+        ));
   }
 }
